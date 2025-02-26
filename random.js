b4781b78b5c976cff4b65d4a43c2bd83c9f3878b
nazarkhamzaev <proctoprocto1@gmail.com>
2025-02-26 10:11:47 +0300

    Html

---
+++ random.js
@@ -0,0 +1,7 @@
+var a=Math.round(Math.random()*3)
+image = new Array();
+image[0]="img0.png"
+image[1]="img1.png"
+image[2]="img2.png"
+image[3]="img3.png"
+document.write ("<img src=" + image[a] + ">");
